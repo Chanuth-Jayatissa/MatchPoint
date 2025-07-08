123626faa3ab4315af1f2fca1cca7c505867f52d
Chanuth Jayatissa <chanuthjayatissa@gmail.com>
2025-07-08 01:13:32 -0400

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1190,7 +1190,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 4,
-    minHeight: 10,
+    minHeight: 5,
   },
   sportIcon: {
     fontSize: 20,
