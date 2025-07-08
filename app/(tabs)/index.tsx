8582f8f54c5e33a1eb72cd02653634f388a4c7c8
Chanuth Jayatissa <chanuthjayatissa@gmail.com>
2025-07-08 01:54:07 -0400

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1417,7 +1417,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 60, // Space for close button
     paddingHorizontal: 24,
-    paddingBottom: 100,
+    paddingBottom: 40,
   },
   profileSection: {
     alignItems: 'center',
